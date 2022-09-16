c57dd5ba845694dda979fdccaa60c88ba6dad44e
noltron000 <noltron000@gmail.com>
2022-09-16 07:51:44 -0700

    Gut boilerplate code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import type {NextPage} from 'next'
 import Head from 'next/head'
-import {Card} from '../components/Card'
 import {Footer} from '../components/Footer'
 import styles from '../styles/Home.module.css'
 
@@ -28,32 +27,6 @@ const Home: NextPage = ( ) => (
 				Get started by editing{' '}
 				<code className={styles.code}>pages/index.tsx</code>
 			</p>
-
-			<div className={styles.grid}>
-				<Card
-					href='https://nextjs.org/docs'
-					title='Documentation &rarr;'
-					description='Find in-depth information about Next.js features and API.'
-				/>
-
-				<Card
-					href='https://nextjs.org/learn'
-					title='Learn &rarr;'
-					description='Learn about Next.js in an interactive course with quizzes!'
-				/>
-
-				<Card
-					href='https://github.com/vercel/next.js/tree/canary/examples'
-					title='Examples &rarr;'
-					description='Discover and deploy boilerplate example Next.js projects.'
-				/>
-
-				<Card
-					href='https://vercel.com/home'
-					title='Deploy &rarr;'
-					description='Instantly deploy your Next.js site to a public URL with Vercel.'
-				/>
-			</div>
 		</main>
 
 		<Footer />

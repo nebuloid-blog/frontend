059116fcc0948d3dd850cc97fc1dc9a28f80bd64
noltron000 <noltron000@gmail.com>
2022-09-19 14:16:42 -0700

    Lint storybook boilerplate

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,41 +1,43 @@
-import React from 'react';
-import type { ComponentStoryFn, ComponentMeta } from '@storybook/react';
+import type {ComponentStoryFn, ComponentMeta} from '@storybook/react'
+import React from 'react'
+import {Button} from './Button'
 
-import { Button } from './Button';
+// More on default export:
+// https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const metadata: ComponentMeta<typeof Button> = {
+	title: 'Example/Button',
+	component: Button,
+	// More on argTypes:
+	// https://storybook.js.org/docs/react/api/argtypes
+	argTypes: {backgroundColor: {control: 'color'}},
+}
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-export default {
-  title: 'Example/Button',
-  component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-} as ComponentMeta<typeof Button>;
+// More on component templates:
+// https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStoryFn<typeof Button> = (args) => <Button {...args} />
+const Primary = Template.bind({ })
+const Secondary = Template.bind({ })
+const Large = Template.bind({ })
+const Small = Template.bind({ })
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStoryFn<typeof Button> = (args) => <Button {...args} />;
-
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
+// More on args:
+// https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  primary: true,
-  label: 'Button',
-};
+	primary: true,
+	label: 'Button',
+}
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
+Secondary.args = {label: 'Button'}
 
-export const Large = Template.bind({});
 Large.args = {
-  size: 'large',
-  label: 'Button',
-};
+	size: 'large',
+	label: 'Button',
+}
 
-export const Small = Template.bind({});
 Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+	size: 'small',
+	label: 'Button',
+}
+
+export {Primary, Secondary, Large, Small}
+export default metadata
--- src/stories/Page.stories.tsx
+++ src/stories/Page.stories.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import type { ComponentStoryFn, ComponentMeta } from '@storybook/react';
-import { within, userEvent } from '@storybook/testing-library';
-import { Page } from './Page';
+import type {ComponentStoryFn, ComponentMeta} from '@storybook/react'
+import {within, userEvent} from '@storybook/testing-library'
+import React from 'react'
+import {Page} from './Page'
 
-export default {
-  title: 'Example/Page',
-  component: Page,
-  parameters: {
-    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
-  },
-} as ComponentMeta<typeof Page>;
+const metadata: ComponentMeta<typeof Page> = {
+	title: 'Example/Page',
+	component: Page,
+	parameters: {
+		// More on Story layout:
+		// https://storybook.js.org/docs/react/configure/story-layout
+		layout: 'fullscreen',
+	},
+}
 
-const Template: ComponentStoryFn<typeof Page> = (args) => <Page {...args} />;
+const Template: ComponentStoryFn<typeof Page> = (args) => <Page {...args} />
+const LoggedIn = Template.bind({ })
+const LoggedOut = Template.bind({ })
 
-export const LoggedOut = Template.bind({});
+// More on interaction testing:
+// https://storybook.js.org/docs/react/writing-tests/interaction-testing
+LoggedIn.play = ({canvasElement}) => {
+	const canvas = within(canvasElement)
+	const loginButton = canvas.getByRole('button', {name: /Log in/ui})
+	userEvent.click(loginButton)
+}
 
-export const LoggedIn = Template.bind({});
-
-// More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
-LoggedIn.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement);
-  const loginButton = await canvas.getByRole('button', { name: /Log in/i });
-  await userEvent.click(loginButton);
-};
+export {LoggedIn, LoggedOut}
+export default metadata

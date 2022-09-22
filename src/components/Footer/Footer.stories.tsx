f242321ef0e038d0b272f6bfbf82d97fc431d001
noltron000 <noltron000@gmail.com>
2022-09-22 09:01:08 -0700

    Write Footer storybook
    This storybook file is a sort of proof/test example to make sure everything works in the setup.

---
+++ src/components/Footer/Footer.stories.tsx
@@ -0,0 +1,19 @@
+import type {ComponentStoryFn, ComponentMeta} from '@storybook/react'
+import React from 'react'
+import {Footer} from './Footer'
+
+// More on default export:
+// https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const metadata: ComponentMeta<typeof Footer> = {
+	title: 'Layout/Footer',
+	component: Footer,
+	parameters: {docs: {page: null}},
+}
+
+// More on component templates:
+// https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStoryFn<typeof Footer> = (args) => <Footer {...args} />
+const Normal = Template.bind({ })
+
+export {Normal}
+export default metadata

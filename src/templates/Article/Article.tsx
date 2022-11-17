85f261a25e8054bbae7158d9d33c180aedf5a57c
noltron000 <noltron000@gmail.com>
2022-11-17 12:16:56 -0800

    Connect API to page props
    This may appear like an intimidating commit, so I'll break it down. 
    
    The most important changes are only in the "get-static-data.ts" files. These connect Nebuloid's Frontend to Nebuloid's Blog.
    
    I've adjusted the frontend to reflect these new static props, and I've added a <Link> in the blog index.

--- src/templates/Article/Article.tsx
+++ src/templates/Article/Article.tsx
@@ -1,17 +1,14 @@
-import type {NextPage} from 'next'
+import type {NextPage, InferGetStaticPropsType} from 'next'
+import type {getStaticProps} from './get-static-data'
 import {DefaultHead} from '@components/DefaultHead'
 import {Footer} from '@components/Footer'
 import {Header} from '@components/Header'
 
-interface Props {
-	article: {
-		html?: string,
-	},
-}
+type Props = InferGetStaticPropsType<typeof getStaticProps>
 
 const Article: NextPage<Props> = ({article}) => {
 	let content
-	if (article.html == null) content = null
+	if (article == null) content = null
 	else content = <article dangerouslySetInnerHTML={{__html: article.html}} />
 
 	return (

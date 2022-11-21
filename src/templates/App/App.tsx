17a5c38ab71c2d6791fe6baa7e2b873a958ed923
noltron000 <noltron000@gmail.com>
2022-11-21 11:25:46 -0800

    Remove extraneous App styles
    We don't need these anymore since the PageWrapper does a similar thing.

--- src/templates/App/App.tsx
+++ src/templates/App/App.tsx
@@ -1,13 +1,10 @@
 import type {NextPage} from 'next'
 import type {AppProps} from 'next/app'
 import {AppWrapper} from './AppWrapper'
-import styles from './app.module.scss'
 
 const App: NextPage<AppProps> = ({Component, pageProps}) => (
 	<AppWrapper>
-		<div className={styles['app-container']}>
-			<Component {...pageProps} />
-		</div>
+		<Component {...pageProps} />
 	</AppWrapper>
 )
 

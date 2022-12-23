d39e968c4bc5ff819a4477d3406297a1e59fc92a
noltron000 <noltron000@gmail.com>
2022-12-22 21:34:22 -0800

    Create a storybook for inline elements

---
+++ src/stories/InlineTags/index.stories.ts
@@ -0,0 +1,8 @@
+import type {Meta, StoryObj} from '@storybook/react'
+import {InlineTags} from './InlineTags'
+
+const metadata: Meta<typeof InlineTags> = {component: InlineTags}
+const Normal: StoryObj<typeof InlineTags> = {args: { }}
+
+export default metadata
+export {Normal}

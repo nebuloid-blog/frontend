c8260093cbf695a724ffea28d1ab4b171ea1c006
noltron000 <noltron000@gmail.com>
2023-01-12 15:13:41 -0800

    Set up global context for color scheme

--- src/templates/App/AppWrapper.tsx
+++ src/templates/App/AppWrapper.tsx
@@ -1,6 +1,8 @@
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import type {NextPage} from 'next'
 import React from 'react'
+import {ColorSchemeProvider} from '@contexts/ColorScheme'
+import {useColorScheme} from '@hooks/use-color-scheme'
 
 interface Props {
 	children: React.ReactNode,
@@ -8,10 +10,17 @@ interface Props {
 
 const queryClient = new QueryClient( )
 
-const AppWrapper: NextPage<Props> = ({children}) => (
-	<QueryClientProvider client={queryClient}>
-		{children}
-	</QueryClientProvider>
-)
+const AppWrapper: NextPage<Props> = ({children}) => {
+	// Set up global state in the global context.
+	const colorSchemeHook = useColorScheme( )
+
+	return (
+		<ColorSchemeProvider value={colorSchemeHook}>
+			<QueryClientProvider client={queryClient}>
+				{children}
+			</QueryClientProvider>
+		</ColorSchemeProvider>
+	)
+}
 
 export {AppWrapper}

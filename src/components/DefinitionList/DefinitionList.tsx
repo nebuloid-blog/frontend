8ccbe619aeb1084e4013c92cb571634bb4f225fb
Nolan Kovacik <noltron000@gmail.com>
2023-02-10 15:02:33 -0800

    Update types in DefinitionList

--- src/components/DefinitionList/DefinitionList.tsx
+++ src/components/DefinitionList/DefinitionList.tsx
@@ -1,45 +1,35 @@
 import React from 'react'
 import type {TextNode} from '@nebuloid-types/text-node'
 
-type DefinitionNode = TextNode | Array<TextNode>
 type HTMLDefinitionListProps = JSX.IntrinsicElements['dl']
 
 interface Props extends HTMLDefinitionListProps {
 	items: Array<[
-		// Terms are inline, and are not wrapped in a <p> tag.
-		// They do get wrapped in a block-oriented <dd> though.
-		// There can be one or more synonyms for a term or word,
-		//  so there can be multiple terms for a single idea.
-		terms: TextNode | Array<TextNode>,
-
-		// Definitions are a collection of one or more <p> tags.
-		// There can be one or more paragraphs for a definition,
-		//  and there can be one or more definitions for a term.
-		definitions: DefinitionNode | Array<DefinitionNode>,
+		terms: Array<TextNode>,
+		definitions: Array<TextNode>,
 	]>,
 }
 
 /* This function presses given "terms" into JSX. */
 const getTermsJSX = (
-	terms: TextNode | Array<TextNode>,
-) => {
+	terms: Array<TextNode>,
+) => (
 	// There can be one or many synonyms for a specific term.
-	terms = Array.isArray(terms) ? terms : [terms]
-	return terms.map((term) => (
+	terms.map((term) => (
 		<dt>{term}</dt>
 	))
-}
+)
+
 
 /* This function presses given "definitions" into JSX. */
 const getDefinitionsJSX = (
-	definitions: DefinitionNode | Array<DefinitionNode>,
-) => {
+	definitions: Array<TextNode>,
+) => (
 	// There can be one or many definitions for a specific term.
-	definitions = Array.isArray(definitions) ? definitions : [definitions]
-	return definitions.map((definition) => (
+	definitions.map((definition) => (
 		<dd>{definition}</dd>
 	))
-}
+)
 
 /* / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
 Definition List
--- src/stories/InlineElements/InlineElements.tsx
+++ src/stories/InlineElements/InlineElements.tsx
@@ -42,36 +42,39 @@ const inlineDefinitionList = [
 		],
 	},
 	{ // Semantically Underlined Text
-		terms: <>underline, (<code>{'<u>'}</code>)</>,
-		definitions: <u>sample</u>,
+		terms: [<>underline, (<code>{'<u>'}</code>)</>],
+		definitions: [<u>sample</u>],
 	},
 	{ // Struck-Out Text
-		terms: <>strikethrough, (<code>{'<s>'}</code>)</>,
-		definitions: <s>sample</s>,
+		terms: [<>strikethrough, (<code>{'<s>'}</code>)</>],
+		definitions: [<s>sample</s>],
 	},
 
 	{ // Quoted Text
-		terms: <>quote, (<code>{'<q>'}</code>)</>,
-		definitions: <q>sample</q>,
+		terms: [<>quote, (<code>{'<q>'}</code>)</>],
+		definitions: [<q>sample</q>],
 	},
 	{ // Abbreviated Phrases
-		terms: <>abbreviation, (<code>{'<abbr>'}</code>)</>,
+		terms: [<>abbreviation, (<code>{'<abbr>'}</code>)</>],
 		definitions: [
 			<abbr>sample</abbr>,
 			<abbr title='with title'>w/title</abbr>,
 		],
 	},
 	{ // Variable Indicators
-		terms: <>variable, (<code>{'<var>'}</code>)</>,
-		definitions: <var>sample</var>,
+		terms: [<>variable, (<code>{'<var>'}</code>)</>],
+		definitions: [<var>sample</var>],
 	},
 
 
 	{ // Human Input Samples
-		terms: <>
-			keyboard/<abbr title='Human Input Device'>HID</abbr>
-			{' '}sample, (<code>{'<kbd>'}</code>)</>,
-		definitions: <kbd>sample</kbd>,
+		terms: [
+			(<>
+				keyboard/<abbr title='Human Input Device'>HID</abbr>
+				{' '}sample, (<code>{'<kbd>'}</code>)
+			</>),
+		],
+		definitions: [<kbd>sample</kbd>],
 	},
 	{ // Code & Output Samples
 		terms: [
@@ -87,22 +90,22 @@ const inlineDefinitionList = [
 	},
 
 	{ // Superscripted Text
-		terms: <>superscript, (<code>{'<sup>'}</code>)</>,
+		terms: [<>superscript, (<code>{'<sup>'}</code>)</>],
 		definitions: [
 			<sup>sample</sup>,
 			<>x<sup>2</sup></>,
 		],
 	},
 	{ // Subscripted Text
-		terms: <>subscript, (<code>{'<sub>'}</code>)</>,
+		terms: [<>subscript, (<code>{'<sub>'}</code>)</>],
 		definitions: [
 			<sub>sample</sub>,
 			<>∂<sub>xy</sub></>,
 		],
 	},
 	{ // Tiny Legal Details
-		terms: <>small/legal, (<code>{'<small>'}</code>)</>,
-		definitions: <small>sample</small>,
+		terms: [<>small/legal, (<code>{'<small>'}</code>)</>],
+		definitions: [<small>sample</small>],
 	},
 
 	{ // Inline Data Denotators

df75c13e9011739498520dc01d33dd9a23a8e5ee
Nolan Kovacik <noltron000@gmail.com>
2023-02-12 10:49:42 -0800

    Apply static HTML

--- src/components/RehypeHTML/RehypeHTML.tsx
+++ src/components/RehypeHTML/RehypeHTML.tsx
@@ -1,4 +1,4 @@
-import {Fragment, useEffect, useState} from 'react'
+import {useEffect, useState} from 'react'
 import {generateJSX} from '@utilities/generate-jsx'
 
 interface Props {
@@ -6,18 +6,30 @@ interface Props {
 }
 
 const RehypeHTML: React.FC<Props> = ({html}) => {
-	const [Content, setContent] = useState(<Fragment />)
+	// This static HTML should already be sanitized
+	//  with the "processHTML" utility function.
+	const [Content, setContent] = useState(
+		<div dangerouslySetInnerHTML={{__html: html}} />,
+	)
+
+	// This will replace static HTML tags
+	//  with dynamic NextJS components, where applicable.
 	useEffect(( ) => {
 		// The only way we can use async operators in useEffect
-		//  is to use an IIFE, which are void by definition.
-		void (async ( ) => {
+		//  is to use an IIFE or a helper function.
+		const loadContent = async ( ) => {
 			const vFile = await generateJSX(html)
 			setContent(vFile.result)
-		})( )
+		}
+
+		void loadContent( )
 	}, [html])
 
-	// If its done, the Rehyped HTML is now ready to serve!
-	// Otherwise it returns a temporary React Fragment.
+	// If its done, the Rehyped JSX is now ready to serve!
+	// Otherwise it returns temporary static HTML;
+	//  this HTML usually only exists for a frame or two.
+	// However, its worth noting that SEO picks up
+	//  on the static HTML (not the generated JSX).
 	return <>{Content}</>
 }
 

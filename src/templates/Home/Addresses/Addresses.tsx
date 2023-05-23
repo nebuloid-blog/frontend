5fe9d15955a7b0d283ee07ff322ffa1740876acb
Nolan Kovacik <noltron000@gmail.com>
2023-05-22 18:49:52 -0700

    Refactor Home template

---
+++ src/templates/Home/Addresses/Addresses.tsx
@@ -0,0 +1,48 @@
+import type {FC} from 'react'
+import styles from '../Home.module.scss'
+
+// Addresses Reference
+const Addresses: FC<Record<never, never>> = ( ) => (
+	<address>
+		<dl>
+			<div className={styles['definition-group']}>
+				<dt>Location</dt>
+				<dd>
+					<i>Los Angeles, California</i>
+				</dd>
+			</div>
+
+			<div className={styles['definition-group']}>
+				<dt>Email</dt>
+				<dd>
+					<a href='mailto:hello@nebuloid.dev'>
+						hello@nebuloid.dev
+					</a>
+				</dd>
+			</div>
+
+			<div className={styles['fill-group']}>
+				<div className={styles['definition-group']}>
+					<dt>Social</dt>
+					<dd>
+						<ul>
+							<li>
+								<a href='https://www.linkedin.com/in/noltron000/'>
+									Linkedin
+								</a>
+							</li>
+
+							<li>
+								<a href='https://github.com/noltron000'>
+									GitHub
+								</a>
+							</li>
+						</ul>
+					</dd>
+				</div>
+			</div>
+		</dl>
+	</address>
+)
+
+export {Addresses}

7308b93a9507238971fc0ffd170bd96488dd8ded
noltron000 <noltron000@gmail.com>
2025-01-24 19:00:57 -0500

    Improve MenuButton

--- src/components/MenuBar/MenuButton/MenuButton.tsx
+++ src/components/MenuBar/MenuButton/MenuButton.tsx
@@ -1,17 +1,25 @@
 import {Button} from '@components/Button'
-import {combineClassNames} from '@utilities/combine-class-names'
-import styles from './MenuButton.module.scss'
-import type {BaseElementType, ButtonProps} from '@components/Button'
 
-type Props<El extends BaseElementType>
+import {
+	combineClassNames,
+} from '@utilities/combine-class-names'
+
+import styles from './MenuButton.module.scss'
+
+import type {
+	BaseElement,
+	ButtonProps,
+} from '@components/Button'
+
+type Props<El extends BaseElement>
 = ButtonProps<El> & {highlight?: boolean}
 
-const MenuButton = <El extends BaseElementType> ({
+const MenuButton = <El extends BaseElement> ({
 	highlight = false,
 	...props
 }: Props<El>) => (
-
 	<li className = {styles.root}>
+		{/* @ts-expect-error: `highlight` shan't be a prop! */}
 		<Button
 			color = 'link'
 			{...props}
